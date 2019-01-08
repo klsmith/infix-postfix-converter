49eb3355b7cc4f7fd29b9aa33fb06779de537955
Kooper Smith <kooper.lewis.smith@gmail.com>
2019-01-07 23:18:49 -0500

    added test for all positive addition staying positive

--- src/test/java/io/github/klsmith/ifpfc/arithmetic/ArithmeticTest.java
+++ src/test/java/io/github/klsmith/ifpfc/arithmetic/ArithmeticTest.java
@@ -21,6 +21,13 @@ public class ArithmeticTest {
     }
 
     @Test
+    public void testAllPositiveAdditionStaysPositive() {
+        qt().forAll(integers().allPositive(), integers().allPositive())
+                .check((a, b) -> new Add(a, b).resolve()
+                        .compareTo(BigDecimal.ZERO) > 0);
+    }
+
+    @Test
     public void testAllSubtract() {
         qt().forAll(integers().all(), integers().all())
                 .asWithPrecursor((a, b) -> new BigDecimal(a).subtract(new BigDecimal(b)))
