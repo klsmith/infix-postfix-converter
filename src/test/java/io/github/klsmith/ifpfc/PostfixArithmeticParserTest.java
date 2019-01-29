c912dba9d0f1db9573f33a676188dd891d2eeebc
Kooper Smith <kooper.lewis.smith@gmail.com>
2019-01-29 14:45:47 -0500

    added support for parsing single nested division

--- src/test/java/io/github/klsmith/ifpfc/PostfixArithmeticParserTest.java
+++ src/test/java/io/github/klsmith/ifpfc/PostfixArithmeticParserTest.java
@@ -195,4 +195,35 @@ public class PostfixArithmeticParserTest {
                 });
     }
 
+    @Test
+    public void testSingleNestedDivision() {
+        qt().forAll(integers().all(), integers().all(), integers().all())
+                .checkAssert((a, b, c) -> {
+                    {
+                        final Arithmetic expected = new Divide(new Divide(a, b), c);
+                        final Arithmetic actual = parser.parse(withSpaces(a, b, "/", c, "/"));
+                        assertEquals(expected, actual);
+                    }
+                    {
+                        final Arithmetic expected = new Divide(a, new Divide(b, c));
+                        final Arithmetic actual = parser.parse(withSpaces(a, b, c, "/", "/"));
+                        assertEquals(expected, actual);
+                    }
+                });
+        qt().forAll(doubles().any(), doubles().any(), doubles().any())
+                .assuming(this::assumeFiniteDoubles)
+                .checkAssert((a, b, c) -> {
+                    {
+                        final Arithmetic expected = new Divide(new Divide(a, b), c);
+                        final Arithmetic actual = parser.parse(withSpaces(a, b, "/", c, "/"));
+                        assertEquals(expected, actual);
+                    }
+                    {
+                        final Arithmetic expected = new Divide(a, new Divide(b, c));
+                        final Arithmetic actual = parser.parse(withSpaces(a, b, c, "/", "/"));
+                        assertEquals(expected, actual);
+                    }
+                });
+    }
+
 }
