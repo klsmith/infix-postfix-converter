bbf3aba13387df51828e018019f1833533a74086
Kooper Smith <kooper.lewis.smith@gmail.com>
2019-02-03 21:53:48 -0500

    added support for infix parsing

---
+++ src/main/java/io/github/klsmith/ifpfc/InfixArithmeticParser.java
@@ -0,0 +1,62 @@
+package io.github.klsmith.ifpfc;
+
+import java.math.BigDecimal;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Scanner;
+
+import io.github.klsmith.ifpfc.arithmetic.Add;
+import io.github.klsmith.ifpfc.arithmetic.Arithmetic;
+import io.github.klsmith.ifpfc.arithmetic.Divide;
+import io.github.klsmith.ifpfc.arithmetic.Multiply;
+import io.github.klsmith.ifpfc.arithmetic.Subtract;
+import io.github.klsmith.ifpfc.arithmetic.Value;
+
+public class InfixArithmeticParser implements ArithmeticParser {
+
+    @Override
+    public Arithmetic parse(String input) {
+        final Deque<Arithmetic> stack = new LinkedList<>();
+        try (final Scanner scanner = new Scanner(input.trim())) {
+            scanner.useDelimiter(" ");
+            while (scanner.hasNext()) {
+                final String token = scanner.next();
+                final Optional<Value> value = parseValue(token);
+                if (value.isPresent()) {
+                    stack.push(value.get());
+                    continue;
+                }
+                final Arithmetic a = stack.pop();
+                final Arithmetic b = parse(scanner.next());
+                switch (token) {
+                    case "+":
+                        stack.push(new Add(a, b));
+                        break;
+                    case "-":
+                        stack.push(new Subtract(a, b));
+                        break;
+                    case "*":
+                        stack.push(new Multiply(a, b));
+                        break;
+                    case "/":
+                        stack.push(new Divide(a, b));
+                    default:
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    private Optional<Value> parseValue(String token) {
+        try {
+            return Optional.of(new Value(new BigDecimal(token)));
+        }
+        catch (NumberFormatException e) {
+            // handled with empty return below
+        }
+        return Optional.empty();
+    }
+
+}
---
+++ src/test/java/io/github/klsmith/ifpfc/InfixArithmeticParserTest.java
@@ -0,0 +1,162 @@
+package io.github.klsmith.ifpfc;
+
+import static io.github.klsmith.ifpfc.TestHelper.withSpaces;
+import static org.junit.Assert.assertEquals;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.doubles;
+import static org.quicktheories.generators.SourceDSL.integers;
+
+import java.util.function.BiFunction;
+
+import org.junit.Test;
+
+import io.github.klsmith.ifpfc.arithmetic.Add;
+import io.github.klsmith.ifpfc.arithmetic.Arithmetic;
+import io.github.klsmith.ifpfc.arithmetic.Divide;
+import io.github.klsmith.ifpfc.arithmetic.Multiply;
+import io.github.klsmith.ifpfc.arithmetic.Subtract;
+import io.github.klsmith.ifpfc.arithmetic.Value;
+
+public class InfixArithmeticParserTest {
+
+    private final ArithmeticParser parser = new InfixArithmeticParser();
+
+    /* Abstract Testing Methods */
+
+    public void testSimple(BiFunction<Arithmetic, Arithmetic, Arithmetic> constructor, String operator) {
+        qt().forAll(integers().all(), integers().all())
+                .checkAssert((a, b) -> {
+                    final Arithmetic expected = constructor.apply(
+                            new Value(a),
+                            new Value(b));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b));
+                    assertEquals(expected, actual);
+                });
+        qt().forAll(doubles().any(), doubles().any())
+                .assuming(TestHelper::assumeFiniteDoubles)
+                .checkAssert((a, b) -> {
+                    final Arithmetic expected = constructor.apply(
+                            new Value(a),
+                            new Value(b));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b));
+                    assertEquals(expected, actual);
+                });
+    }
+
+    private void testSingleNested(BiFunction<Arithmetic, Arithmetic, Arithmetic> constructor, String operator) {
+        qt().forAll(integers().all(), integers().all(), integers().all())
+                .checkAssert((a, b, c) -> {
+                    final Arithmetic expected = constructor.apply(
+                            constructor.apply(new Value(a), new Value(b)),
+                            new Value(c));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b, operator, c));
+                    assertEquals(expected, actual);
+                });
+        qt().forAll(doubles().any(), doubles().any(), doubles().any())
+                .assuming(TestHelper::assumeFiniteDoubles)
+                .checkAssert((a, b, c) -> {
+                    final Arithmetic expected = constructor.apply(
+                            constructor.apply(new Value(a), new Value(b)),
+                            new Value(c));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b, operator, c));
+                    assertEquals(expected, actual);
+                });
+    }
+
+    private void testDoubleNested(BiFunction<Arithmetic, Arithmetic, Arithmetic> constructor, String operator) {
+        qt().forAll(integers().all(), integers().all(), integers().all(), integers().all())
+                .checkAssert((a, b, c, d) -> {
+                    final Arithmetic expected = constructor.apply(
+                            constructor.apply(
+                                    constructor.apply(
+                                            new Value(a),
+                                            new Value(b)),
+                                    new Value(c)),
+                            new Value(d));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b, operator, c, operator, d));
+                    assertEquals(expected, actual);
+                });
+        qt().forAll(doubles().any(), doubles().any(), doubles().any(), doubles().any())
+                .assuming(TestHelper::assumeFiniteDoubles)
+                .checkAssert((a, b, c, d) -> {
+                    final Arithmetic expected = constructor.apply(
+                            constructor.apply(
+                                    constructor.apply(
+                                            new Value(a),
+                                            new Value(b)),
+                                    new Value(c)),
+                            new Value(d));
+                    final Arithmetic actual = parser.parse(withSpaces(a, operator, b, operator, c, operator, d));
+                    assertEquals(expected, actual);
+                });
+    }
+
+    /* Addition Tests */
+
+    @Test
+    public void testSimpleAddition() {
+        testSimple(Add::new, Add.SYMBOL);
+    }
+
+    @Test
+    public void testSingleNestedAddition() {
+        testSingleNested(Add::new, Add.SYMBOL);
+    }
+
+    @Test
+    public void testDoubleNestedAddition() {
+        testDoubleNested(Add::new, Add.SYMBOL);
+    }
+
+    /* Subtraction Tests */
+
+    @Test
+    public void testSimpleSubtraction() {
+        testSimple(Subtract::new, Subtract.SYMBOL);
+    }
+
+    @Test
+    public void testSingleNestedSubtraction() {
+        testSingleNested(Subtract::new, Subtract.SYMBOL);
+    }
+
+    @Test
+    public void testDoubleNestedSubtraction() {
+        testDoubleNested(Subtract::new, Subtract.SYMBOL);
+    }
+
+    /* Multiplication Tests */
+
+    @Test
+    public void testSimpleMultiplication() {
+        testSimple(Multiply::new, Multiply.SYMBOL);
+    }
+
+    @Test
+    public void testSingleNestedMultiplication() {
+        testSingleNested(Multiply::new, Multiply.SYMBOL);
+    }
+
+    @Test
+    public void testDoubleNestedMultiplication() {
+        testDoubleNested(Multiply::new, Multiply.SYMBOL);
+    }
+
+    /* Division Tests */
+
+    @Test
+    public void testSimpleDivision() {
+        testSimple(Divide::new, Divide.SYMBOL);
+    }
+
+    @Test
+    public void testSingleNestedDivision() {
+        testSingleNested(Divide::new, Divide.SYMBOL);
+    }
+
+    @Test
+    public void testDoubleNestedDivision() {
+        testDoubleNested(Divide::new, Divide.SYMBOL);
+    }
+
+}
