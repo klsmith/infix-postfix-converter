98accade93892269f3681d0f55567a7ef84506e0
Kooper Smith <kooper.lewis.smith@gmail.com>
2019-02-05 14:31:36 -0500

    refactored and improved coverage for direct tests

--- src/test/java/io/github/klsmith/ifpfc/InfixToPostfixConverterTest.java
+++ src/test/java/io/github/klsmith/ifpfc/InfixToPostfixConverterTest.java
@@ -15,6 +15,7 @@ import io.github.klsmith.ifpfc.arithmetic.Arithmetic;
 import io.github.klsmith.ifpfc.arithmetic.Divide;
 import io.github.klsmith.ifpfc.arithmetic.Multiply;
 import io.github.klsmith.ifpfc.arithmetic.Subtract;
+import io.github.klsmith.ifpfc.arithmetic.Value;
 
 public class InfixToPostfixConverterTest {
 
@@ -22,24 +23,52 @@ public class InfixToPostfixConverterTest {
     private final ArithmeticStringWriter infixWriter = new InfixArithmeticStringWriter();
     private final ArithmeticStringWriter postfixWriter = new PostfixArithmeticStringWriter();
 
+    /* Abstract Testing Methods */
+
     private void testSimple(BiFunction<Arithmetic, Arithmetic, Arithmetic> constructor,
             String operator) {
         qt().forAll(integers().all(), integers().all(), integers().all(), integers().all())
                 .checkAssert((a, b, c, d) -> {
-                    final String input = withSpaces(a, "+", b, "+", c, "+", d);
+                    final String input = withSpaces(a, operator, b, operator, c, operator, d);
                     final String actual = converter.toPostfix(input);
-                    final String expected = withSpaces(a, b, "+", c, "+", d, "+");
+                    final String expected = withSpaces(a, b, operator, c, operator, d, operator);
                     assertEquals(expected, actual);
                 });
         qt().forAll(doubles().any(), doubles().any(), doubles().any(), doubles().any())
                 .assuming(TestHelper::assumeFiniteDoubles)
                 .checkAssert((a, b, c, d) -> {
-                    final String input = withSpaces(a, "+", b, "+", c, "+", d);
+                    final String input = withSpaces(a, operator, b, operator, c, operator, d);
                     final String actual = converter.toPostfix(input);
-                    final String expected = withSpaces(a, b, "+", c, "+", d, "+");
+                    final String expected = withSpaces(a, b, operator, c, operator, d, operator);
                     assertEquals(expected, actual);
                 });
     }
+
+    private void testDirect(BiFunction<Arithmetic, Arithmetic, Arithmetic> constructor) {
+        qt().forAll(integers().all(), integers().all(), integers().all(), integers().all())
+                .checkAssert((a, b, c, d) -> {
+                    final Arithmetic arithmetic = constructor.apply(
+                            constructor.apply(new Value(a), new Value(b)),
+                            constructor.apply(new Value(c), new Value(d)));
+                    final String input = infixWriter.write(arithmetic);
+                    final String actual = converter.toPostfix(input);
+                    final String expected = postfixWriter.write(arithmetic);
+                    assertEquals(expected, actual);
+                });
+        qt().forAll(doubles().any(), doubles().any(), doubles().any(), doubles().any())
+                .assuming(TestHelper::assumeFiniteDoubles)
+                .checkAssert((a, b, c, d) -> {
+                    final Arithmetic arithmetic = constructor.apply(
+                            constructor.apply(new Value(a), new Value(b)),
+                            constructor.apply(new Value(c), new Value(d)));
+                    final String input = infixWriter.write(arithmetic);
+                    final String actual = converter.toPostfix(input);
+                    final String expected = postfixWriter.write(arithmetic);
+                    assertEquals(expected, actual);
+                });
+    }
+
+    /* Addition Tests */
 
     @Test
     public void testSimpleAddition() {
@@ -47,9 +76,23 @@ public class InfixToPostfixConverterTest {
     }
 
     @Test
+    public void testDirectAddition() {
+        testDirect(Add::new);
+    }
+
+    /* Subtraction Tests */
+
+    @Test
     public void testSimpleSubtraction() {
         testSimple(Subtract::new, Subtract.SYMBOL);
     }
+
+    @Test
+    public void testDirectSubtraction() {
+        testDirect(Subtract::new);
+    }
+
+    /* Multiplication Test */
 
     @Test
     public void testSimpleMultiplication() {
@@ -57,17 +100,20 @@ public class InfixToPostfixConverterTest {
     }
 
     @Test
+    public void testDirectMultiplication() {
+        testDirect(Multiply::new);
+    }
+
+    /* Division Tests */
+
+    @Test
     public void testSimpleDivision() {
         testSimple(Divide::new, Divide.SYMBOL);
     }
 
     @Test
-    public void testDirect() {
-        final Arithmetic arithmetic = new Add(new Add(1, 2), new Add(3, 4));
-        final String input = infixWriter.write(arithmetic);
-        final String actual = converter.toPostfix(input);
-        final String expected = postfixWriter.write(arithmetic);
-        assertEquals(expected, actual);
+    public void testDirectDivision() {
+        testDirect(Divide::new);
     }
 
 }
